98ace7d39a31b55811fc81b3464a181b1882b4a9
= <=>
2023-07-06 06:16:59 -0700

    Refactored package composition

--- src/main/java/com/cloudydaiyz/learningcenter/Main.java
+++ src/main/java/com/cloudydaiyz/learningcenter/Main.java
@@ -1,5 +1,6 @@
-package com.cloudydaiyz;
+package com.cloudydaiyz.learningcenter;
 
+// This is just a test Main class
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
